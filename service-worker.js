c33ba78816af0f6812a7d736a0653aa1729ce18a
Isaiah Carter <izay.carter0509@gmail.com>
2020-01-17 18:19:27 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/5-4-1-bobs-place-izaycarter/precache-manifest.21ed85ffb45c704d94d2ea3dfa52e919.js"
+  "/Zays-Restaurant/precache-manifest.c40cc564115b6b1f073de422949981e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/5-4-1-bobs-place-izaycarter/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zays-Restaurant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

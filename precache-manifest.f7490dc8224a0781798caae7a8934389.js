ed36563e99d541636932704baa457fd5ecc6c2b0
Isaiah Carter <izay.carter0509@gmail.com>
2020-01-17 13:58:29 -0500

    Updates

---
+++ precache-manifest.f7490dc8224a0781798caae7a8934389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653df126f423ed42324e1724d62645d1",
+    "url": "/Zays-Restaurant/index.html"
+  },
+  {
+    "revision": "19521510610963ae8ad9",
+    "url": "/Zays-Restaurant/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9b9baec0d44b4d929231",
+    "url": "/Zays-Restaurant/static/css/main.e8932447.chunk.css"
+  },
+  {
+    "revision": "19521510610963ae8ad9",
+    "url": "/Zays-Restaurant/static/js/2.9b89cfa5.chunk.js"
+  },
+  {
+    "revision": "9b9baec0d44b4d929231",
+    "url": "/Zays-Restaurant/static/js/main.b5249379.chunk.js"
+  },
+  {
+    "revision": "00d6723c22b2a9371173",
+    "url": "/Zays-Restaurant/static/js/runtime-main.b11e18bb.js"
+  }
+]);
